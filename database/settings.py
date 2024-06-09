2b55a59005df00c62f9bd560648a6b472fa94b8a
S1delnikov <92618363+S1delnikov@users.noreply.github.com>
2024-06-09 22:57:06 +0300

    Update settings.py

--- database/settings.py
+++ database/settings.py
@@ -2,9 +2,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+import psycopg2
+
+ URL_DATABASE = psycopg2.connect(
+    host="147.45.239.35",
+    database="default_db",
+    user="gen_user",
+    password="<FA;}JeFy\pF13"
+)
+
 
 # URL_DATABASE = 'postgresql://cactus:99936022MCmc@localhost:5432/task_tracker' # для локальной разработки
-URL_DATABASE = "postgresql://gen_user:%3CFA%3B%7DJeFy%5CpF13@147.45.239.35:5432/default_db"
+# URL_DATABASE = "postgresql://gen_user:%3CFA%3B%7DJeFy%5CpF13@147.45.239.35:5432/default_db"
 # URL_DATABASE = 'postgresql+psycopg2://cactus_hdvq_user:yq6Ur3SbBluUa8bG24DWLFCwnMLDwrpm@dpg-cp39d1g21fec73b3qoqg-a.frankfurt-postgres.render.com/cactus_hdvq?sslmode=require'
 # URL_DATABASE = 'postgresql+psycopg2://cactus:rH1jdjGcVGEuKQ7wSFg20dqEjn7fXcGI@dpg-cnq6s0v79t8c7396mmo0-a.frankfurt-postgres.render.com/task2?sslmode=require'
 
