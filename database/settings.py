34350b2f04a0297b2a3919dedd3d1cfc64852a63
S1delnikov <92618363+S1delnikov@users.noreply.github.com>
2024-06-09 22:26:36 +0300

    Update settings.py

--- database/settings.py
+++ database/settings.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 
 # URL_DATABASE = 'postgresql://cactus:99936022MCmc@localhost:5432/task_tracker' # для локальной разработки
-URL_DATABASE = "postgresql+psycopg2://gen_user:%3CFA%3B%7DJeFy%5CpF13@192.168.0.4:5432/default_db?sslmode=require"
+URL_DATABASE = "postgresql://gen_user:%3CFA%3B%7DJeFy%5CpF13@192.168.0.4:5432/default_db?sslmode=require"
 # URL_DATABASE = 'postgresql+psycopg2://cactus_hdvq_user:yq6Ur3SbBluUa8bG24DWLFCwnMLDwrpm@dpg-cp39d1g21fec73b3qoqg-a.frankfurt-postgres.render.com/cactus_hdvq?sslmode=require'
 # URL_DATABASE = 'postgresql+psycopg2://cactus:rH1jdjGcVGEuKQ7wSFg20dqEjn7fXcGI@dpg-cnq6s0v79t8c7396mmo0-a.frankfurt-postgres.render.com/task2?sslmode=require'
 
