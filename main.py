2123a54c60c252efe8f312b20a1f9682dc977565
S1delnikov <92618363+S1delnikov@users.noreply.github.com>
2024-06-09 19:32:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://s1delnikov-task-tracker-frontend-772d.twc1.net/"],
+    allow_origins=["https://s1delnikov-task-tracker-frontend-772d.twc1.net"],
     allow_credentials=True, 
     allow_methods=["*"],
     allow_headers=["*"],
